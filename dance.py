86c7b7644ee051dacb70cf5bc6566e76f1ed699f
Azmi Kamis <azmi.stuff@gmail.com>
2015-04-11 14:47:16 +0800

    modified

--- dance.py
+++ dance.py
@@ -1,15 +1,25 @@
 __author__ = 'azmi'
 
+
+def checkpossibility(bh, gh):
+    if len(gh) < len(bh):
+        return False
+
+    if bh[0] <= gh[0]:
+        return False
+
+    for a, b in zip(bh, gh):
+        if b > a:
+            return False
+
+    return True
+
 t = int(raw_input())
 for _ in range(t):
     m, n = map(int, raw_input().split())
     boysheights = sorted(map(int, raw_input().split()))
     girlsheights = sorted(map(int, raw_input().split()))
 
-    possible = True
-    for a, b in zip(boysheights, girlsheights):
-        if b > a:
-            possible = False
-            break
+    possible = checkpossibility(boysheights, girlsheights)
 
     print 'YES' if possible else 'NO'
