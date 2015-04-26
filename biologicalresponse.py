83117ba7b9cebd484364c0b6ce9f0404a1b57eee
azmikamis <azmi.stuff@gmail.com>
2015-04-26 18:13:39 +0800

    kaggle

---
+++ biologicalresponse.py
@@ -0,0 +1,20 @@
+__author__ = 'azmi'
+
+from sklearn.ensemble import RandomForestClassifier
+from numpy import genfromtxt, savetxt
+
+# create the training & test sets, skipping the header row with [1:]
+dataset = genfromtxt(open('/home/azmi/data/train.csv', 'r'), delimiter=',', dtype='f8')[1:]
+target = [x[0] for x in dataset]
+train = [x[1:] for x in dataset]
+test = genfromtxt(open('/home/azmi/data/test.csv', 'r'), delimiter=',', dtype='f8')[1:]
+
+# create and train the random forest
+# multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(train, target)
+predicted_probs = [[index + 1, x[1]] for index, x in enumerate(rf.predict_proba(test))]
+
+savetxt('/home/azmi/data/submission.csv', predicted_probs, delimiter=',', fmt='%d,%f',
+        header='MoleculeId,PredictedProbability', comments='')
+
